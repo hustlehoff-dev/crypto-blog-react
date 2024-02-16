ec0955c9643a052d3e438a314c9b0a7e1220d064
hustlehoff-dev <148590992+hustlehoff-dev@users.noreply.github.com>
2024-02-16 09:27:45 +0100

    Add json db

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,32 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blog, setBlog] = useState([
-    {
-      title: "How to center a div?",
-      body: "Centering a div is quite an easy...",
-      author: "Hustlehoff",
-      id: 1,
-    },
-    {
-      title: "Trying new things",
-      body: "You do not know until you try yourself that...",
-      author: "Apple",
-      id: 2,
-    },
-    {
-      title: "An awful shit",
-      body: "Lets discuss something ,that is really...",
-      author: "Hustlehoff",
-      id: 3,
-    },
-  ]);
+  const [blog, setBlog] = useState(null);
 
   const handleDelete = (id) => {
     const newBlog = blog.filter((blogs) => blogs.id !== id);
     setBlog(newBlog);
   };
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blog")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlog(data);
+      });
+  }, []);
 
   return (
     <div className="Home">
