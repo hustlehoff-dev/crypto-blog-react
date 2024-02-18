722b92d7609a38fc9b9367cd368b0d7a3c10ab36
hustlehoff-dev <148590992+hustlehoff-dev@users.noreply.github.com>
2024-02-18 11:03:13 +0100

    POST

--- src/Create.tsx
+++ src/Create.tsx
@@ -1,16 +1,57 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [body, setBody] = useState("");
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    setIsPending(true);
+    fetch("http://localhost:8000/blog", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("New blog saved.");
+      setIsPending(false);
+      Navigate("/");
+    });
+  };
+
   return (
     <div className="create">
       <h2>+ Add new blog</h2>
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <label htmlFor="">Title:</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
         <label htmlFor="">Author:</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
 
-        <label htmlFor="">Blog content:</label>
-        <textarea name="" id="" cols="30" rows="10" required></textarea>
+        <label htmlFor="">Blog body:</label>
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
 
         <select name="" id="">
           <option value="private">Private</option>
